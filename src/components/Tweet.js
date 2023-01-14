2ec89099ec15f54c8fc34c034812f15d01731dca
António Cortez <aamcortez@gmail.com>
2023-01-14 14:48:14 +0000

    lab done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,39 +1,32 @@
-function Tweet() {
-  return (
-    <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+function Tweet(props) {
+   return (
+      <div className="tweet">
+         <ProfileImage image={props.tweet.user.image} />
 
-          <span className="timestamp">Nov 30, 2020</span>
-        </div>
+         <div className="body">
+            <div className="top">
+               <User
+                  name={props.tweet.user.name}
+                  handle={props.tweet.user.handle}
+               />
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+               <Timestamp timestamp={props.tweet.timestamp} />
+            </div>
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+            <Message message={props.tweet.message} />
+
+            <Actions />
+         </div>
+
+         <i className="fas fa-ellipsis-h"></i>
       </div>
-
-      <i className="fas fa-ellipsis-h"></i>
-    </div>
-  );
+   );
 }
 
 export default Tweet;
